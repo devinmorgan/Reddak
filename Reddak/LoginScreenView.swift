2182da6972fa3002bb9464738fcf8f92264b7513
Devin Morgan <devinm@mit.edu>
2016-08-03 19:13:35 -0400

    Created new View and ViewController to create the app login/register page

---
+++ Reddak/LoginScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginScreenView.swift
+//  Reddak
+//
+//  Created by Devin Morgan on 8/3/16.
+//  Copyright © 2016 Devin Morgan. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
