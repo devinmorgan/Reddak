075ef283eb0e3450a2499d2849dc552b78d415c7
Devin Morgan <devinm@mit.edu>
2016-08-03 23:41:09 -0400

    Added the solid background color behind the background gradients

--- Reddak/LoginScreenViewController.swift
+++ Reddak/LoginScreenViewController.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class LoginScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        // Do any additional setup after loading the view, typically from a nib.
+        let backgroundColor = UIColor(red: 59.0/255.0, green: 47.0/255.0, blue: 99/255.0, alpha: 1.0)
+        view.backgroundColor = backgroundColor
+    }
 
 }
