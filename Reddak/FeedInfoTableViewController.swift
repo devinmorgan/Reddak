9e2a996c354c86710abeff2132ed717e9d5e4913
Devin Morgan <devinm@mit.edu>
2016-08-18 15:56:55 -0400

    Started making the feed information TableViewController programatically

---
+++ Reddak/FeedInfoTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  FeedInfoTableViewController.swift
+//  Reddak
+//
+//  Created by Devin Morgan on 8/18/16.
+//  Copyright © 2016 Devin Morgan. All rights reserved.
+//
+
+import UIKit
+
+class FeedInfoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        switch section {
+        case 0: // Description Section
+            return 0
+        case 1: // Statis & Info
+            return 3
+        case 2: // Membership Status
+            return 1
+        default:
+            return 0
+        }
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
