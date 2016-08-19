1476f58e78265444b3cde2796d426946ffc6dd6e
Devin Morgan <devinm@mit.edu>
2016-08-18 23:34:39 -0400

    Made progress on the feed information scene

--- Reddak/FeedInfoTableViewController.swift
+++ Reddak/FeedInfoTableViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class FeedInfoTableViewController: UITableViewController {
+    
+    let purpleColor = UIColor.init(red: 110.0/255.0, green: 93.0/255.0, blue: 168.0/255.0, alpha: 1)
+    let greyColor = UIColor.init(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1)
+    let lightGreyColor = UIColor.init(white: 240.0/255.0, alpha: 1.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -38,11 +41,70 @@ class FeedInfoTableViewController: UITableViewController {
         default:
             return 0
         }
+        
     }
 
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let boldFont = UIFont.boldSystemFontOfSize(18)
+        
+        let label = UILabel.init()
+        label.textColor = purpleColor
+        label.font = boldFont
+        label.backgroundColor = UIColor.clearColor()
+        
+        switch section {
+        case 0:
+            label.text = "Description"
+        case 1:
+            label.text = "Stats & Info"
+        case 2:
+            label.text = "Membership Status"
+        default:
+            label.text = "Default"
+        }
+        
+        return label
+    }
+    
+    override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let normalFont = UIFont.init(name: "System-Regular", size: 14)
+        let textContent = "This is the main feed for MIT. Every MIT member can view and make posts here."
+        
+        let textView = UITextView.init()
+        textView.textColor = greyColor
+        textView.font = normalFont
+        textView.text = textContent
+        textView.backgroundColor = UIColor.clearColor()
+        textView.textContainer.lineFragmentPadding = 0
+        textView.editable = false
+        
+        
+        switch section {
+        case 0:
+            return textView
+        default:
+            let view = UIView.init()
+            view.backgroundColor = UIColor.clearColor()
+            return view
+        }
+
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 22.0
+    }
+    
+    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 50.0
+        default:
+            return 20.0
+        }
+    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("ReuseIdentifier", forIndexPath: indexPath)
 
         // Configure the cell...
 
